34d20a59820aef13654bc0c3b162491751757c1d
tanaka2468 <144119069+tanaka2468@users.noreply.github.com>
2024-09-09 11:28:47 +0000

    Add files via upload

---
+++ PROGPOE/Academic Finance.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROGPOE
+{
+    /// <summary>
+    /// Interaction logic for Academic_Finance.xaml
+    /// </summary>
+    public partial class Academic_Finance : Window
+    {
+        public Academic_Finance()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PROGPOE/PC.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROGPOE
+{
+    /// <summary>
+    /// Interaction logic for PC.xaml
+    /// </summary>
+    public partial class PC : Window
+    {
+        public PC()
+        {
+            InitializeComponent();
+        }
+    }
+}
